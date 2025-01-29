f26c1720bebebd162a2367390a26477d1be7b912
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2025-01-29 23:11:30 +0000

    acabandoVistas

--- src/vistas/vistapartidas.jsx
+++ src/vistas/vistapartidas.jsx
@@ -1,9 +1,18 @@
-export function TablaPartidas(){
-  console.log("Cargado tabla partidas")
-  return(
-    <>
-        <h1>Tabla</h1>
-    </>
-  );
-};
+import React from "react";
+import TablaPartidas from "../TablasPartidas";
 
+
+export function VistaPartidas() {
+  return (
+    <div className="text-light">
+      <header className="d-flex align-items-center justify-content-center">
+        <h1 className="mt-5">Partidas</h1>
+      </header>
+      <main className="container mt-5 bg-opacity-50 bg-dark p-2">
+        <TablaPartidas />
+      </main>
+    </div>
+  );
+}
+
+export default VistaPartidas;
